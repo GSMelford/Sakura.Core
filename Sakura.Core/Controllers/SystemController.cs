ff31efa3c5bf810c43f911237c8231c0a21b0dbd
Maksym Fedorenko <m.fedorenko@upswot.com>
2022-04-09 12:06:22 +0300

    [Add] SystemController -> CheckSystem()

---
+++ Sakura.Core/Controllers/SystemController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sakura.Core.Controllers;
+
+[ApiController]
+public class SystemController : Controller
+{
+    private readonly ILogger<SystemController> _logger;
+    
+    public SystemController(ILogger<SystemController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("check")]
+    public IActionResult CheckSystem()
+    {
+        _logger.LogInformation("The Sakura system works");
+        return Ok();
+    }
+}
